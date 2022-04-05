dfc50e2e34b5a3ac2f21169439f9f32a8a5141ec
Tdicarus <ngt679@gmail.com>
2022-04-05 14:50:29 +0700

    gagagigo

--- app/src/main/java/ngothuctridat/baikt1/baikt1/MainActivity.java
+++ app/src/main/java/ngothuctridat/baikt1/baikt1/MainActivity.java
@@ -13,39 +13,39 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private TextView toastlogin,tvattempt;
     private EditText edtPass,edtUN;
+    Integer i = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         edtUN = (EditText) findViewById(R.id.edtUN);
         edtPass = (EditText) findViewById(R.id.edtPass);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         tvattempt = (TextView) findViewById(R.id.tvattempt);
         toastlogin = (TextView) findViewById(R.id.toastlogin);
 
+
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Integer t = i-1;
+                i = t;
                 String username = edtUN.getText().toString().trim();
                 String password = edtPass.getText().toString().trim();
                 String s ="maicuongtho";
                 String a ="Cntt60ntu!";
-                if(TextUtils.isEmpty(username)||TextUtils.isEmpty(password))
+                while (i >= 0)
                 {
-                    Toast.makeText(MainActivity.this, "fill User and password", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                else if(username == s && password != a || username != s && password == a )
-                {
-                    Toast.makeText(MainActivity.this, "User and password is wrong", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                else
-                {
-                Toast.makeText(MainActivity.this, "User and password is correct", Toast.LENGTH_LONG).show();
-                return;
+                    if (username.equals(s) && password.equals(a)) {
+                        Toast.makeText(MainActivity.this, "User and password is correct", Toast.LENGTH_LONG).show();
+                        return;
+                    } else {
+                        Toast.makeText(MainActivity.this, "User and password is wrong", Toast.LENGTH_LONG).show();
+                        tvattempt.setText(t.toString());
+                        return;
+                    }
                 }
             }
         });
