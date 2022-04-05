cb1e35eaa183399b5ca798277cf5fbb80bbaa9b6
Tdicarus <ngt679@gmail.com>
2022-04-05 14:04:50 +0700

    dododo

--- app/src/main/java/ngothuctridat/baikt1/baikt1/MainActivity.java
+++ app/src/main/java/ngothuctridat/baikt1/baikt1/MainActivity.java
@@ -2,12 +2,26 @@ package ngothuctridat.baikt1.baikt1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private TextView toastlogin,tvattempt;
+    private EditText edtPass,edtUN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUN = (EditText) findViewById(R.id.edtUN);
+        edtPass = (EditText) findViewById(R.id.edtPass);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        tvattempt = (TextView) findViewById(R.id.tvattempt);
+        toastlogin = (TextView) findViewById(R.id.toastlogin);
+
+        btnLogin.setOnClickListener();
     }
 }
