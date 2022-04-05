c1888dff9cf450c6b8f03f41df872b27ce0da2ce
Tdicarus <ngt679@gmail.com>
2022-04-05 14:19:45 +0700

    Update MainActivity.java

--- app/src/main/java/ngothuctridat/baikt1/baikt1/MainActivity.java
+++ app/src/main/java/ngothuctridat/baikt1/baikt1/MainActivity.java
@@ -2,9 +2,12 @@ package ngothuctridat.baikt1.baikt1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
@@ -22,6 +25,29 @@ public class MainActivity extends AppCompatActivity {
         tvattempt = (TextView) findViewById(R.id.tvattempt);
         toastlogin = (TextView) findViewById(R.id.toastlogin);
 
-        btnLogin.setOnClickListener();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edtUN.getText().toString().trim();
+                String password = edtPass.getText().toString().trim();
+                String s ="maicuongtho";
+                String a ="Cntt60ntu!";
+                if(TextUtils.isEmpty(username)||TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(MainActivity.this, "fill User and password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else if(username == s && password != a || username != s && password == a )
+                {
+                    Toast.makeText(MainActivity.this, "User and password is wrong", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                Toast.makeText(MainActivity.this, "User and password is correct", Toast.LENGTH_LONG).show();
+                return;
+                }
+            }
+        });
     }
 }
